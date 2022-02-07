f1c7e5749c502dc1261755faaa04b7cd089c7570
Legrain Samuel <samuel.legrain@bstorm.be>
2022-02-07 16:32:25 +0100

    Add OrdersController (EntityFramework controller)

--- TF.WEB.API.Demo/Controllers/OrdersController.cs
+++ TF.WEB.API.Demo/Controllers/OrdersController.cs
@@ -30,10 +30,61 @@ namespace TF.WEB.API.Demo.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Order>> GetOne(string id)
         {
-            Order order = await _context.Orders.FindAsync(id);
+            try
+            {
+                Order order = await _context.Orders.FindAsync(id);
+                if (order is null) return NotFound();
+                return order;
+            }
+            catch (BadHttpRequestException e)
+            {
+                return BadRequest();
+            }
 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                if (_context.Orders.Any(o => o.Reference == order.Reference)) return Conflict();
+                throw e;
+            }
+            return CreatedAtAction("GetOne", new { id = order.Reference }, new { result = order });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(string id, Order order)
+        {
+            if (id != order.Reference) return BadRequest();
+
+            _context.Entry(order).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                if (!_context.Orders.Any(o => o.Reference == order.Reference)) return Conflict();
+                throw e;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DelOrder(string id)
+        {
+            Order order = await _context.Orders.FindAsync(id);
             if (order is null) return NotFound();
-            return order;
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
